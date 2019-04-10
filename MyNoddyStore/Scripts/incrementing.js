6ed0d4c058558f5ef6edd0ee01ebaadeec9e417b
sajeewa <saj2@saj.com>
2019-04-10 17:49:49 +0100

    Updating items all working jump point to next bit

--- MyNoddyStore/Scripts/incrementing.js
+++ MyNoddyStore/Scripts/incrementing.js
@@ -1,4 +1,4 @@
-$('.minus-btn').on('click', function (e) { /*todo remove this reference ??*/
+$('.minus-btn').on('click', function (e) { /*todo remove this reference */
     e.preventDefault();
     var $this = $(this);
     var $input = $this.closest('div').find('input');
@@ -28,35 +28,4 @@ $('.plus-btn').on('click', function (e) {
 
     $input.val(value);
 });
-//$(function () {
 
-//    $("form div").append('<div class="inc button">+</div><div class="dec button">-</div>');
-
-//});
-
-
-//$(function () {
-
-//  $(".numbers-row").append('<div class="inc button">+</div><div class="dec button">-</div>');
-
-//  $(".button").on("click", function() {
-
-//    var $button = $(this);
-//    var oldValue = $button.parent().find("input").val();
-
-//    if ($button.text() == "+") {
-//  	  var newVal = parseFloat(oldValue) + 1;
-//  	} else {
-//	   // Don't allow decrementing below zero
-//      if (oldValue > 0) {
-//        var newVal = parseFloat(oldValue) - 1;
-//	    } else {
-//        newVal = 0;
-//      }
-//	  }
-
-//    $button.parent().find("input").val(newVal);
-
-//  });
-
-//});
